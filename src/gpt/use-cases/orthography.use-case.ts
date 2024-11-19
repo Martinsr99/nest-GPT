aa3b0694ddfdd7eaec5846779201059d2cda3f3e
Martinsr99 <silesreche.martin@gmail.com>
2024-11-19 18:41:45 +0100

    feat: Add parameters on prompt on openai

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -9,11 +9,32 @@ export const orthographyCheckUseCase = async (openai: OpenAI,options: Options) =
     const {prompt} = options;
 
     const completion = await openai.chat.completions.create({
-      messages: [{role:"system",content: "You are a helpful assistant"},{role:'user',content:prompt}],
-      model: "chatgpt-4o-latest"
+      messages: [{role:"system",content: 
+        `
+        Te serán proveídos textos en español con posibles errores ortográficos y gramaticales, las palabras usadas deben de existir en el diccionario de la real academia española, debes de responder en formato JSON,
+
+        tu tarea es corregirlos y retornar el feedback de las solicitudes, tambien debes de dar un porcentaje de acierto por el usuario,
+
+        Si no hay errores, debes de retornar un mensaje de felicitaciones.
+
+        Ejemplo de salida:
+        {
+          userScore: number,
+          errors: string[], //['error -> solucion']
+          message: string // Usa emojis y texto para felicitar al usuario
+        }
+        `
+      },
+      {role:'user',content:prompt}],
+      model: "chatgpt-4o-latest",
+      temperature: 0.3,
+      max_tokens: 150,
+      response_format: {
+        type: 'json_object',
+      }
     })
 
     console.log(completion)
-    return completion.choices[0]
+    return JSON.parse(completion.choices[0].message.content)
     
 };
